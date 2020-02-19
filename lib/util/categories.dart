45e9df501f28537777f64f5310b9edced53ebaf3
ashokpokharel977 <ashokpokharel977@gmail.com>
2020-02-19 22:41:50 +0545

    added bug fixes

---
+++ lib/util/categories.dart
@@ -0,0 +1,59 @@
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+List categories = [
+  {
+    "name": "Beer",
+    "icon": FontAwesomeIcons.beer,
+    "items": 5
+  },
+  {
+    "name": "Wishkey",
+    "icon": FontAwesomeIcons.glassWhiskey,
+    "items": 20
+  },
+  {
+    "name": "Vodka",
+    "icon": FontAwesomeIcons.wineBottle,
+    "items": 9
+  },
+  {
+    "name": "Rum",
+    "icon": FontAwesomeIcons.pizzaSlice,
+    "items": 5
+  },
+  {
+    "name": "Wine",
+    "icon": FontAwesomeIcons.dribbble,
+    "items": 15
+  },
+  {
+    "name": "Non Alcholoic",
+    "icon": FontAwesomeIcons.glassCheers,
+    "items": 5
+  },
+  {
+    "name": "Gin",
+    "icon": FontAwesomeIcons.glassMartini,
+    "items": 20
+  },
+  {
+    "name": "Ciders",
+    "icon": FontAwesomeIcons.glassMartiniAlt,
+    "items": 9
+  },
+  {
+    "name": "Brandy",
+    "icon": FontAwesomeIcons.wineGlass,
+    "items": 5
+  },
+  {
+    "name": "Cigrattes",
+    "icon": FontAwesomeIcons.youtube,
+    "items": 15
+  },
+  {
+    "name": "Snacks",
+    "icon": FontAwesomeIcons.facebook,
+    "items": 15
+  },
+];
